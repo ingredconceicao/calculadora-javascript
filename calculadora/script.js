0dc720165f4e9170c1b542328b3e6b8b3051977b
Ingred Conceição <84211250+ingredconceicao@users.noreply.github.com>
2023-04-10 23:11:02 -0300

    Add files via upload

---
+++ calculadora/script.js
@@ -0,0 +1,5 @@
+const numeros = document.querySelectorAll('input')
+const telaResultado = document.querySelector('#result')
+function displaynum(n1){
+  Calculator.text1.value = Calculator.text1.value + n1
+}
